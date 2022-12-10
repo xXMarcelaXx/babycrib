6cc3b77f8827261111f693f4f40adb0ffce56c1e
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2022-12-10 17:56:43 -0600

    oiokey

--- app/Http/Controllers/GetFeedsController.php
+++ app/Http/Controllers/GetFeedsController.php
@@ -42,7 +42,7 @@ class GetFeedsController extends Controller
     public function getFeed($key_feed,$keyAio)
     {
         $def="none";
-        $response = Http::withHeaders(['X-AIO-Key'=>"aio_KsXH94NweMDJIIFxPKUZo0hETUuw"])->get(
+        $response = Http::withHeaders(['X-AIO-Key'=>"aio_Zisf07EMSnlO5gfHIrOpr86XyLAN"])->get(
             $this->url.$this->user."feeds/".$key_feed);
         if($response->ok()){
              $def = $response->object()->last_value;
