e41b2c6190e77a56b2b558a611e7e52ae6ac1937
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2022-12-10 18:18:24 -0600

    chocholuis

--- app/Http/Controllers/GetFeedsController.php
+++ app/Http/Controllers/GetFeedsController.php
@@ -42,7 +42,7 @@ class GetFeedsController extends Controller
     public function getFeed($key_feed,$keyAio)
     {
         $def="none";
-        $response = Http::withHeaders(['X-AIO-Key'=>"aio_trxR994GNTW6kuT1cikcjZ9aEFWT"])->get(
+        $response = Http::withHeaders(['X-AIO-Key'=>"aio_iDmb80gPxzoGSfwPyQ2mncWGF4ku"])->get(
             $this->url.$this->user."feeds/".$key_feed);
         if($response->ok()){
              $def = $response->object()->last_value;
